4309423886999b72a798c9de5eab05658dc3db15
Joylon Lester Dsouza <148479728+lesterdza10@users.noreply.github.comit>
2026-09-19 00:08:10 +0530

    feat: integrate Leaflet for map functionality in search page, add geocoding for pickup and drop locations, and implement custom markers

--- src/app/user/search/page.tsx
+++ src/app/user/search/page.tsx
@@ -1,11 +1,24 @@
 "use client";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Search } from "lucide-react";
 import { motion } from "motion/react";
 import React from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import SearchMap from "@/components/SearchMap";
 
 function page() {
   const router = useRouter();
+  const params = useSearchParams();
+
+  const [pickup, setPickup] = React.useState(params.get("pickup") || "");
+  const [drop, setDrop] = React.useState(params.get("drop") || "");
+  const [km, setKm] = React.useState<number>();
+  const mobile = params.get("mobile") || "";
+  const pickupLat = Number(params.get("pickupLat") || "");
+  const pickupLon = Number(params.get("pickupLon") || "");
+  const dropLat = Number(params.get("dropLat") || "");
+  const dropLon = Number(params.get("dropLon") || "");
+  const vehicle = params.get("vehicle") || "";
+
   return (
     <div className="min-h-screen bg-zinc-100 text-zinc-100 overflow-x-hidden">
       <div className="absolute top-5 left-5 z-50">
@@ -19,6 +32,17 @@ function page() {
         >
           <ArrowLeft size={24} className="text-zinc-900" />
         </motion.button>
+      </div>
+      <div className="relative w-full h-[52vh] z-0">
+        <SearchMap
+          pickup={pickup}
+          drop={drop}
+          onChange={(pickup, drop) => {
+            setPickup(pickup);
+            setDrop(drop);
+          }}
+          onDistance={setKm}
+        />
       </div>
     </div>
   );
